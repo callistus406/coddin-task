9badd164a28fc56c9721c658fe63275b4f4dbbce
CALLISTUS <callistus406@gmail.com>
2024-01-18 07:15:55 -0500

    fixed

--- src/api/v1/routes/register.ts
+++
@@ -1,5 +0,0 @@
-import express from "express";
-import RegisterController from "../controllers/registrationCtrl";
-const router = express.Router();
-router.post("/register", RegisterController.createAdmin);
-export default router;
